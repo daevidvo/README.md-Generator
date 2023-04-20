d3be446ec55369fafd9399f489db9acf1e83faff
daevidvo <itsdavidvo@gmail.com>
2023-04-19 19:09:48 -0700

    finished index.js functions

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 // TODO: Include packages needed for this application
 const fs = require('fs')
 const inq = require('inquirer')
-const genMD = require('./utils/generateMarkdown')
+const genMD = require('./utils/generateMarkdown');
+const generateMarkdown = require('./utils/generateMarkdown');
 
 // TODO: Create an array of questions for user input
 // require str input of title, description, install instructions, usage information, contribution guidelines, test instructions, github username, email, and choose license that the user would like to use. 
@@ -24,7 +25,7 @@ const questions = [
     {
         type: 'input',
         message: 'What are the usage information of your project?',
-        name: 'title',
+        name: 'usage',
     },
     {
         type: 'input',
@@ -50,17 +51,21 @@ const questions = [
         type: 'checkbox',
         message: 'What is the title of your project?',
         choices: ['Apache 2.0', 'GNU General Public License v3.0', 'MIT License', 'BSD 2-Clause Simplified License', 'BSD-3 Clause New or Revised License', 'Unlicensed'],
-        name: 'title',
+        name: 'license',
     },
 
 ];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(data) {
+    fs.appendFile('generated_readme.md', data, (err)=>{err?console.error(err):console.log('generated_readme.md created')})
+}
 
 // TODO: Create a function to initialize app
 function init() {
-    inquirer.prompt(questions).then
+    inq.prompt(questions).then((r)=>{
+        writeToFile(generateMarkdown(r))
+    })
 }
 
 // Function call to initialize app
